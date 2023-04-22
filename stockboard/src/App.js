e31ab87281ed818301d07ba5cd7a7d2dda716dd4
Lesly Williams <lesly.williams001@mymdc.net>
2023-04-22 18:15:57 -0400

    update build

--- stockboard/src/App.js
+++ stockboard/src/App.js
@@ -1,17 +1,16 @@
-import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider, Container } from "@mui/material";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-import {useState} from "react";
-
-
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider, Container } from "@mui/material";
 
 function App() {
-  const [searchValue, setSearchValue] = useState('TSLA')
+  const [searchValue, setSearchValue] = useState("TSLA");
   const [theme, colorMode] = useMode();
   const [lineColor, setLineColor] = useState("#20c997");
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
@@ -20,21 +19,24 @@ function App() {
           <Sidebar />
           <Container>
             <main className={"content"}>
-              <Topbar searchValue={searchValue} setSearchValue={setSearchValue} lineColor={lineColor} setLineColor={setLineColor} />
-              <Router>
+              <Topbar
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+                lineColor={lineColor}
+                setLineColor={setLineColor}
+              />
+              <Router basename={process.env.PUBLIC_URL}>
                 <Routes>
-                  <Route path="/" element={<Dashboard searchValue={searchValue} lineColor={lineColor} setLineColor={setLineColor} />} />
-                  {/*<Route path="/team" element={<Dashboard />} />*/}
-                  {/*<Route path="/contacts" element={<Dashboard />} />*/}
-                  {/*<Route path="/invoices" element={<Dashboard />} />*/}
-                  {/*<Route path="/form" element={<Dashboard />} />*/}
-                  {/*<Route path="/bar" element={<Dashboard />} />*/}
-                  {/*<Route path="/pie" element={<Dashboard />} />*/}
-                  {/*<Route path="/line" element={<Dashboard />} />*/}
-                  {/*<Route path="/faq" element={<Dashboard />} />*/}
-                  {/*<Route path="/geography" element={<Dashboard />} />*/}
-                  {/*<Route path="/calendar" element={<Dashboard />} />*/}
-
+                  <Route
+                    path="/"
+                    element={
+                      <Dashboard
+                        searchValue={searchValue}
+                        lineColor={lineColor}
+                        setLineColor={setLineColor}
+                      />
+                    }
+                  />
                 </Routes>
               </Router>
             </main>
