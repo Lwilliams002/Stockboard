afdb5f5cad2117529f8c3028b311a86bec3f08e6
Lesly Williams <lesly.williams001@mymdc.net>
2023-04-23 19:34:00 -0400

    server connection

--- application.py
+++ application.py
@@ -1,13 +1,22 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import yfinance as yf
 import pandas as pd
 from datetime import date, timedelta
 
-app = Flask(__name__)
-CORS(app)
+application = Flask(__name__)
+CORS(application)
 
-@app.route('/stocks', methods=['GET'])
+
+@application.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
+
+
+@cross_origin()
+@application.route('/stocks', methods=['GET'])
 def get_stocks():
     # Get query parameters
     symbol = request.args.get('symbol', 'TSLA')
@@ -15,13 +24,8 @@ def get_stocks():
     end_date = request.args.get('end_date', date.today().strftime("%Y-%m-%d"))
 
     # Fetch stock price data
-    data = yf.download(symbol, start=start_date, end=end_date, interval='30m', auto_adjust=True)
-
-    # Calculate daily returns
-    data[f'{symbol} Daily Return'] = data['Close'].pct_change()
-
-    # Drop any rows with missing data
-    data = data.dropna()
+    url = f"http://Server-env.eba-v2awp3cu.us-east-1.elasticbeanstalk.com/stocks?symbol={symbol}&start_date={start_date}&end_date={end_date}"
+    data = pd.read_json(url)
 
     # Convert DataFrame to dictionary
     response_data = data.to_dict(orient='index')
@@ -29,7 +33,8 @@ def get_stocks():
     # Convert Timestamp objects to strings
     response_data = {str(k): v for k, v in response_data.items()}
 
-    return jsonify(response_data)
+    return jsonify(response_data), 200, {'Access-Control-Allow-Origin': '*'}
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
